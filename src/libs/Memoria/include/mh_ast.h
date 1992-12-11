e3bc780cc16954ccfc25c01ae2d5a5da28bb802c
carr <carr>
1992-12-11 11:19:51 +0000

    Initial revision

---
+++ src/libs/Memoria/include/mh_ast.h
@@ -0,0 +1,29 @@
+#ifndef mh_ast_h
+#define mh_ast_h
+
+#ifndef ast_h
+#include <fort/ast.h>
+#endif
+#ifndef astutil_h
+#include <fort/astutil.h>
+#endif
+#ifndef astlist_h
+#include <fort/astlist.h>
+#endif
+#ifndef astcons_h
+#include <fort/astcons.h>
+#endif
+#ifndef astnode_h
+#include <fort/astnode.h>
+#endif
+#ifndef astsel_h
+#include <fort/astsel.h>
+#endif
+#ifndef asttree_h
+#include <fort/asttree.h>
+#endif
+#ifndef astrec_h
+#include <fort/astrec.h>
+#endif
+
+#endif
