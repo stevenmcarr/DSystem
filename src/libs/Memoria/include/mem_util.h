66b218e3f1129e4a4300b9870046b3bfc613bce4
carr <carr>
1993-06-15 14:07:11 +0000

    added routines for memory costs

--- src/libs/Memoria/include/mem_util.h
+++ src/libs/Memoria/include/mem_util.h
@@ -1,4 +1,4 @@
-/* $Id: mem_util.h,v 1.5 1992/12/11 11:19:49 carr Exp $ */
+/* $Id: mem_util.h,v 1.6 1993/06/15 14:07:11 carr Exp $ */
 
 #ifndef mem_util_h
 #define mem_util_h
@@ -6,6 +6,7 @@
 #ifndef general_h
 #include <general.h>         /* for Generic, EXTERN */
 #endif
+
 #ifndef Arena_h
 #include <Arena.h>            /* for arena_type */
 #endif 
@@ -21,6 +22,21 @@
 #ifndef el_h
 #include <el.h>               /* for EDGE_INDEX */
 #endif
+
+#ifndef dg_h
+#include <dg.h>
+#endif
+
+#ifndef dt_h
+#include <dt.h>
+#endif
+
+#ifndef dp_h
+#include <dp.h>
+#endif
+
+typedef struct loop_struct model_loop;
+typedef enum LocalityType;
 
 typedef struct list_struct *listp;
 
@@ -42,6 +58,16 @@ typedef struct copyinfotype {
   SymDescriptor symtab;
  } copy_info_type;
 
+typedef struct StatsStruct {
+  PedInfo ped;
+  float   flops,
+          mops;
+  int     level;
+  Boolean UseCache;
+  model_loop *loop_data;
+  int         loop;
+ } StatsInfoType;
+
 #define Head(l) (l.head)
 
 #define Length1(l) (l.head == l.tail)
@@ -56,14 +82,9 @@ typedef struct copyinfotype {
 EXTERN(AST_INDEX, ut_get_stmt,(AST_INDEX node));
 EXTERN(AST_INDEX, ut_tree_copy_with_type,(AST_INDEX node,int index,
 					arena_type *ar));
-EXTERN(void, ut_new_tail,(listnode *list,EDGE_INDEX edge,
-				  arena_type *ar));
-EXTERN(void, ut_empty_new,(listnode *list));
-EXTERN(listp, ut_get_start,(listnode list));
-EXTERN(listp, ut_remove,(listp p,listnode *list));
-EXTERN(void, ut_free_list,(listnode list));
 
 EXTERN(int, floor_ab,(int a, int b));
+EXTERN(int, ceil_ab,(int a, int b));
 EXTERN(int, mod,(int a,int b));
 EXTERN(int, gcd,(int a,int b));
 EXTERN(int, lcm,(int a,int b));
@@ -74,5 +95,13 @@ EXTERN(int, ut_check_div,(AST_INDEX node,Generic contains_div));
 EXTERN(void, ut_update_bounds,(AST_INDEX loop,AST_INDEX copy,
 				       int val));
 EXTERN(int, ut_init_copies,(AST_INDEX node,
-				    Generic copy_info));
+			    Generic copy_info));
+
+EXTERN(int, ut_ComputeBalance,(AST_INDEX     node,
+			       StatsInfoType *Stats));
+EXTERN(LocalityType, ut_GetReferenceType, (AST_INDEX  node,
+					   model_loop *loop_data,
+					   int        loop,
+					   PedInfo    ped));
+EXTERN(void, ut_GetSubscriptText, (AST_INDEX Node, char *Text));
 #endif
