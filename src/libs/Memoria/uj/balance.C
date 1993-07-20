0b802cebe40986317bb1204ac15e2961d9472da2
carr <carr>
1993-07-20 16:34:05 +0000

    added cache balance computation

--- src/libs/Memoria/uj/balance.C
+++ src/libs/Memoria/uj/balance.C
@@ -1,10 +1,30 @@
-/* $Id: balance.C,v 1.3 1992/12/17 14:50:57 carr Exp $ */
-#include <balance.h>
+/* $Id: balance.C,v 1.4 1993/07/20 16:34:05 carr Exp $ */
 
 /****************************************************************************/
 /*                                                                          */
+/*    File:   balance.C                                                     */
+/*                                                                          */
+/*    Description:  Functions to compute the balance of a loop given a set  */
+/*                  of unroll values.                                       */
 /*                                                                          */
 /****************************************************************************/
+
+#include <balance.h>
+#include <compute_uj.h>
+#include <mem_util.h>
+
+
+/****************************************************************************/
+/*                                                                          */
+/*    Function:     get_coeff_index                                         */
+/*                                                                          */
+/*    Input:        x - unroll amount                                       */
+/*                                                                          */
+/*    Description:  Determine which coefficient to use in computing balance */
+/*                  distance 0, distance 1 or 0 or all distances            */
+/*                                                                          */
+/****************************************************************************/
+
 
 static int get_coeff_index(int x)
 
@@ -20,15 +40,27 @@ static int get_coeff_index(int x)
        }
   }
 
+
+/****************************************************************************/
+/*                                                                          */
+/*    Function:     mh_fp_register_pressure                                 */
+/*                                                                          */
+/*    Input:        reg_coeff - coefficients to the register pressure       */
+/*                              pressure formula                            */
+/*                  scalar_coeff - coefficients for scalar array refs       */
+/*                  x1,x2 - unroll amounts for the two loops.               */
+/*                                                                          */
+/*    Description:  Determine the number of floating point registers        */
+/*                  required by a loop, given the unroll amounts for two    */
+/*                  loops.  This comes from Carr's thesis.                  */
+/*                                                                          */
+/****************************************************************************/
+
+
 int mh_fp_register_pressure(int reg_coeff[4][3][3],
 			    int *scalar_coeff,
 			    int x1,
 			    int x2)
-
-/****************************************************************************/
-/*                                                                          */
-/*                                                                          */
-/****************************************************************************/
 
   {
    int regs,cindex1,cindex2;
@@ -46,14 +78,24 @@ int mh_fp_register_pressure(int reg_coeff[4][3][3],
      return(regs);
   }
 
-int mh_addr_register_pressure(int addr_coeff[4][3][3],
-			      int x1,
-			      int x2)
 
 /****************************************************************************/
 /*                                                                          */
+/*    Function:     mh_addr_register_pressure                               */
+/*                                                                          */
+/*    Input:        addr_coeff - coefficients for determining address       */
+/*                               register pressure                          */
+/*                  x1,x2 - unroll amounts for two loops                    */
+/*                                                                          */
+/*    Description:  Determine the address-register pressure in a loop given */
+/*                  two unroll amounts.                                     */
 /*                                                                          */
 /****************************************************************************/
+
+
+int mh_addr_register_pressure(int addr_coeff[4][3][3],
+			      int x1,
+			      int x2)
 
   {
    int regs,cindex1,cindex2;
@@ -67,15 +109,27 @@ int mh_addr_register_pressure(int addr_coeff[4][3][3],
      return(regs);
   }
 
+
+/****************************************************************************/
+/*                                                                          */
+/*    Function:     mh_loop_balance                                         */
+/*                                                                          */
+/*    Input:        mem_coeff - coefficients for formula to compute the     */
+/*                              number of memory references in a loop.      */
+/*                  flops - number of floating-point ops in a loop.         */
+/*                  x1,x2 - unroll amount for two loops.                    */
+/*                                                                          */
+/*    Description:  Determine the ratio of memory ops to flops in a loop    */
+/*                  nest given unroll amounts for two loops.  This is from  */
+/*                  Carr's thesis.                                          */
+/*                                                                          */
+/****************************************************************************/
+
+
 float mh_loop_balance(int   mem_coeff[4][3][3],
 		      int   flops,
 		      int   x1,
 		      int   x2)
-
-/****************************************************************************/
-/*                                                                          */
-/*                                                                          */
-/****************************************************************************/
 
   {
    float num,denom;
@@ -92,3 +146,47 @@ float mh_loop_balance(int   mem_coeff[4][3][3],
   }
 
     
+float mh_loop_balance_cache(int       mem_coeff[4][3][3],
+			    CoeffType PrefetchCoeff,
+			    int       flops,
+			    int       LineSize,
+			    int       x1,
+			    int       x2)
+
+  {
+   float P0,PC,PI,denom;
+   int cindex1,cindex2;
+
+     cindex1 = get_coeff_index(x1);
+     cindex2 = get_coeff_index(x2);
+
+     P0 = ((float)PrefetchCoeff.V0[0][cindex1][cindex2] * x1 * x2) / (float)LineSize +
+          (float)PrefetchCoeff.V0[1][cindex1][cindex2] * x2 * ceil_ab(x1,LineSize) +
+	  (float)PrefetchCoeff.V0[2][cindex1][cindex2] * x1 * ceil_ab(x2,LineSize) +
+	  (float)PrefetchCoeff.V0[3][cindex1][cindex2] * x1 * x2;
+     PC = ((float)(PrefetchCoeff.VC[0][0][cindex1][cindex2] * x1 * x2 + 
+		   PrefetchCoeff.VC[0][1][cindex1][cindex2] * x1 + 
+		   PrefetchCoeff.VC[0][2][cindex1][cindex2] * x2 + 
+		   PrefetchCoeff.VC[0][3][cindex1][cindex2]) / LineSize) +
+          (float)(PrefetchCoeff.VC[1][0][cindex1][cindex2] * x2 * ceil_ab(x1,LineSize) +
+		  PrefetchCoeff.VC[1][1][cindex1][cindex2] * ceil_ab(x1,LineSize) +
+		  ceil_ab(PrefetchCoeff.VC[1][3][cindex1][cindex2],LineSize)) +
+          (float)(PrefetchCoeff.VC[2][0][cindex1][cindex2] * x1 * ceil_ab(x2,LineSize) +
+		  PrefetchCoeff.VC[2][2][cindex1][cindex2] * ceil_ab(x2,LineSize) +
+		  ceil_ab(PrefetchCoeff.VC[1][3][cindex1][cindex2],LineSize)) +
+	  (float)(PrefetchCoeff.VC[3][0][cindex1][cindex2] * x1 * x2 + 
+		  PrefetchCoeff.VC[3][1][cindex1][cindex2] * x1 + 
+		  PrefetchCoeff.VC[3][2][cindex1][cindex2] * x2 + 
+		  PrefetchCoeff.VC[3][3][cindex1][cindex2]);
+     PI = ((float)(PrefetchCoeff.VI[0][1][cindex1][cindex2] * x1 +
+		   PrefetchCoeff.VI[0][2][cindex1][cindex2] * x2 +
+		   PrefetchCoeff.VI[0][3][cindex1][cindex2]) / (float)LineSize) +
+          ((float)(PrefetchCoeff.VI[1][2][cindex1][cindex2] * x2))/(float)LineSize +
+          ((float)(PrefetchCoeff.VI[2][1][cindex1][cindex2] * x1))/(float)LineSize +
+          ((float)(PrefetchCoeff.VI[0][1][cindex1][cindex2] * x1 +
+		   PrefetchCoeff.VI[0][2][cindex1][cindex2] * x2 +
+		   PrefetchCoeff.VI[0][3][cindex1][cindex2]));
+     denom = (float)(flops * x1 * x2);
+     return((P0 + PC + PI)/denom);
+  }
+     
