0c7c0aae5229bca54b3692b0ea464d1bfc060ef7
carr <carr>
1994-05-31 15:01:36 +0000

    Initial revision

---
+++ src/libs/Memoria/li/Reversal.C
@@ -0,0 +1,151 @@
+/****************************************************************/
+/*                                                              */
+/*   File:   Reversal.C                                      */
+/*                                                              */
+/****************************************************************/
+
+#ifndef general_h
+#include <general.h>
+#endif 
+
+#include <mh.h>
+#include <mh_ast.h>
+#include <fort/walk.h>
+#include <pt_util.h>
+#include <mh_config.h>
+#include <MemoryOrder.h>
+#include <Reversal.h>
+
+#ifndef header_h
+#include <header.h>
+#endif
+
+#ifndef dt_h
+#include <dt.h>
+#endif
+
+#ifndef gi_h
+#include <fort/gi.h>
+#endif 
+
+#ifndef stats_h
+#include <stats.h>
+#endif 
+
+#ifndef analyze_h
+#include <analyze.h>
+#endif 
+
+#ifndef shape_h
+#include <shape.h>
+#endif 
+
+#ifndef mem_util_h
+#include <mem_util.h>
+#endif 
+
+#ifndef mark_h
+#include <mark.h>
+#endif 
+
+#ifndef LoopStats_h
+#include <LoopStats.h>
+#endif 
+
+#ifndef dg_h
+#include	<dg.h>		/* dg_add_edge()		*/
+#endif 
+	      
+
+
+/****************************************************************/
+/*                                                              */
+/*   Function:   ReversalMakesInterchangeLegal                  */
+/*                                                              */
+/****************************************************************/
+
+static Boolean ReversalMakesInterchangeLegal(UtilList *EdgeList,
+					     PedInfo  ped,
+					     int      level)
+  {
+   UtilNode *Edge;
+   DG_Edge  *dg;
+   int      dist;
+   EDGE_INDEX edge;
+
+    
+     dg = dg_get_edge_structure( PED_DG(ped));
+     Edge = UTIL_HEAD(EdgeList);
+     while(Edge != NULLNODE)
+       {
+	edge = UTIL_NODE_ATOM(Edge);
+	dist = gen_get_dt_DIS(&dg[edge],level);
+
+	switch(dist) {
+	  case DDATA_GE:
+	  case DDATA_GT: 
+	  case DDATA_NE:  break;
+
+
+	  case DDATA_ANY: 
+	  case DDATA_LT:
+	  case DDATA_LE:  return(false);
+	                  break;
+
+	  case DDATA_ERROR: return(false);
+			    break;
+
+	  default:
+	          if (dist > 0)
+		    return(false);
+	       }
+	Edge = UTIL_NEXT(Edge);
+       }
+     return(true);
+  }
+
+
+static void PerformLoopReversal(model_loop *loop_data,
+				int        loop)
+
+  {
+   AST_INDEX from,to,step,control;
+
+     control = gen_DO_get_control(loop_data[loop].node);
+     from = gen_INDUCTIVE_get_rvalue1(control);
+     to = gen_INDUCTIVE_get_rvalue2(control);
+     step = gen_INDUCTIVE_get_rvalue3(control);
+     gen_INDUCTIVE_put_rvalue1(control,AST_NIL);
+     gen_INDUCTIVE_put_rvalue2(control,AST_NIL);
+     gen_INDUCTIVE_put_rvalue3(control,AST_NIL);
+     gen_INDUCTIVE_put_rvalue1(control,to);
+     gen_INDUCTIVE_put_rvalue2(control,from);
+     if (step == AST_NIL)
+       gen_INDUCTIVE_put_rvalue3(control,pt_gen_int(-1));
+     else
+       gen_INDUCTIVE_put_rvalue3(control,
+				 pt_simplify_expr(pt_gen_mul(pt_gen_int(-1),step)));
+     loop_data[loop].reversed = true;
+  }
+     
+
+/****************************************************************/
+/*                                                              */
+/*   Function:  li_ComputeMemoryOrder                           */
+/*                                                              */
+/****************************************************************/
+
+Boolean li_LoopReversal(model_loop  *loop_data,
+			int         loop,
+			UtilList    *EdgeList,
+			PedInfo     ped)
+
+  {
+   if (ReversalMakesInterchangeLegal(EdgeList,ped,loop_data[loop].level))
+     {
+      PerformLoopReversal(loop_data,loop);
+      return(true);
+     }
+   else
+     return(false);
+  }
---
+++ src/libs/Memoria/li/Reversal.h
@@ -0,0 +1,13 @@
+
+#ifndef Reversal_h
+#define Reversal_h
+
+#include <general.h>
+#include <mh.h>
+
+EXTERN(Boolean, li_LoopReversal, (model_loop  *loop_data,
+				  int         loop,
+				  UtilList    *EdgeList,
+				  PedInfo     ped));
+
+#endif 
