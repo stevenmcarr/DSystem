949da599bdbe23bfd464370218a84388d4f3e4eb
carr <carr>
1994-06-13 16:13:32 +0000

    fixed -s option

--- src/libs/Memoria/include/memory_menu.h
+++ src/libs/Memoria/include/memory_menu.h
@@ -1,23 +1,24 @@
-/* $Id: memory_menu.h,v 1.3 1994/06/13 10:33:15 carr Exp $ */
+/* $Id: memory_menu.h,v 1.4 1994/06/13 16:13:32 carr Exp $ */
 
 #ifndef memory_menu_h
 #define memory_menu_h
 
 #define NO_SELECT		-1
-#define INTERSTATS              0
-#define INTERCHANGE             1
-#define	SCALAR_REP 		2
-#define UNROLLSTATS             3
-#define	UNROLL_AND_JAM  	4
-#define UNROLL_SCALAR           5
-#define LI_SCALAR               6
-#define LI_UNROLL               7
-#define MEM_ALL			8 
-#define PREFETCH                9
-#define ANNOTATE                10
-#define LI_FUSION		11
-#define CACHE_ANALYSIS          12
-#define MEM_SIZE		13    /* MUST BE LAST! */
+#define INTERCHANGE             0
+#define	SCALAR_REP 		1
+#define	UNROLL_AND_JAM  	2
+#define UNROLL_SCALAR           3
+#define LI_SCALAR               4
+#define LI_UNROLL               5
+#define MEM_ALL			5 
+#define PREFETCH                7
+#define ANNOTATE                8
+#define LI_FUSION		9
+#define CACHE_ANALYSIS          10
+#define LI_STATS                11
+#define UJ_STATS                12
+#define SR_STATS                13
+#define MEM_SIZE		14    /* MUST BE LAST! */
 
 #define ARENAS 1
 
