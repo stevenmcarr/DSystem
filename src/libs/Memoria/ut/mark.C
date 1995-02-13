804e62c8309d6e375e95734c550c880ca532d6c5
carr <carr>
1995-02-13 15:16:48 +0000

    added read_long and format statement handling in setting surrounding do value

--- src/libs/Memoria/ut/mark.C
+++ src/libs/Memoria/ut/mark.C
@@ -1,4 +1,4 @@
-/* $Id: mark.C,v 1.7 1994/07/20 11:33:28 carr Exp $ */
+/* $Id: mark.C,v 1.8 1995/02/13 15:16:48 carr Exp $ */
 
 
 /****************************************************************************/
@@ -103,6 +103,9 @@ int ut_mark_do_pre(AST_INDEX       stmt,
    else if (is_read_short(stmt))
      walk_expression(gen_READ_SHORT_get_data_vars_LIST(stmt),(WK_EXPR_CLBACK)
 		     set_surrounding_do,(WK_EXPR_CLBACK)NOFUNC,pre_info);
+   else if (is_read_long(stmt))
+     walk_expression(gen_READ_LONG_get_io_LIST(stmt),(WK_EXPR_CLBACK)
+		     set_surrounding_do,(WK_EXPR_CLBACK)NOFUNC,pre_info);
    else if (is_logical_if(stmt))
      walk_expression(gen_LOGICAL_IF_get_rvalue(stmt),(WK_EXPR_CLBACK)set_surrounding_do,(WK_EXPR_CLBACK)NOFUNC,
 		     pre_info);
@@ -119,7 +122,7 @@ int ut_mark_do_pre(AST_INDEX       stmt,
      walk_expression(gen_RETURN_get_rvalue(stmt),(WK_EXPR_CLBACK)set_surrounding_do,(WK_EXPR_CLBACK)NOFUNC,
 		     pre_info);
    else if (is_if(stmt) || is_continue(stmt) || is_goto(stmt) ||
-            is_assigned_goto(stmt));
+            is_assigned_goto(stmt) || is_format(stmt));
    else if (executable_stmt(stmt))
      { 
       char errmsg[30];
