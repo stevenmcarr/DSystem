214fcb70b5f496a5ed699fef65cfe068eac07932
carr <carr>
1998-02-18 19:24:34 +0000

    fixed error in exponentiation

--- src/libs/f2i/data.C
+++ src/libs/f2i/data.C
@@ -1,4 +1,4 @@
-/* $Id: data.C,v 1.1 1997/04/28 20:18:07 carr Exp $ */
+/* $Id: data.C,v 1.2 1998/02/18 19:24:34 carr Exp $ */
 /******************************************************************************/
 /*        Copyright (c) 1990, 1991, 1992, 1993, 1994 Rice University          */
 /*                           All Rights Reserved                              */
@@ -19,13 +19,21 @@
 #include <libs/f2i/mnemonics.h>
 
 #include <math.h>
+/*  move bit_masks[] to data.c and make static  */
+static int bit_masks[32] = {	0x00000001, 0x00000002, 0x00000004, 0x00000008,
+			0x00000010, 0x00000020, 0x00000040, 0x00000080,
+			0x00000100, 0x00000200, 0x00000400, 0x00000800,
+			0x00001000, 0x00002000, 0x00004000, 0x00008000,
+			0x00010000, 0x00020000, 0x00040000, 0x00080000,
+			0x00100000, 0x00200000, 0x00400000, 0x00800000,
+			0x01000000, 0x02000000, 0x04000000, 0x08000000,
+			0x10000000, 0x20000000, 0x40000000, 0x80000000 };
+
+
 
 # define	FALSE	0
 # define	TRUE	1
 
-
-/* forward declarations */
-extern long int	*bit_masks;
 
 
 /* structure for data statements  */
