bac7143a59cde67eda8fc4f1af5fdaba10e03120
carr <carr>
1998-07-07 19:26:55 +0000

    updated recurrence computation to be at node level not statement level

--- src/libs/Memoria/ut/Recurrence.h
+++ src/libs/Memoria/ut/Recurrence.h
@@ -2,17 +2,19 @@
 #define Recurrence_h
 
 #include <libs/Memoria/include/mh.h>
+#include <libs/Memoria/include/ASTToIntMap.h>
 
 #define INFINITY 100000000
 
 class Recurrence {
 
-  int NumberOfStmts;
-  AST_INDEX *Stmts;
+  int NumberOfNodes;
+  AST_INDEX *Nodes;
   AST_INDEX loop;
   PedInfo ped;
   int *T;
   int level;
+  ASTToIntMap *ASTMap;
 
   void ComputeT();
 
@@ -23,18 +25,20 @@ public:
     {
       int i;
       
-      delete Stmts;
+      delete ASTMap;
+      delete Nodes;
       delete T;
     }
 
-  Boolean IsReferenceOnRecurrence(AST_INDEX ref);
-  void PutStmt(AST_INDEX stmt) {Stmts[NumberOfStmts] = stmt;}
-  int GetNumberOfStmts() {return NumberOfStmts;}
-  void IncrementNumberOfStmts() {++NumberOfStmts;}
+  Boolean IsReferenceOnRecurrence(AST_INDEX node);
+  void PutNode(AST_INDEX node) {Nodes[NumberOfNodes] = node;}
+  int GetNumberOfNodes() {return NumberOfNodes;}
+  void IncrementNumberOfNodes() {++NumberOfNodes;}
+  ASTToIntMap *GetASTMap() {return ASTMap;}
    
 };
 
-STATIC(int, CountStatements,(AST_INDEX stmt,int level, int *size));
-STATIC(int, InitializeStmtMapping,(AST_INDEX stmt,int level, Recurrence *R));
+STATIC(int, CountNodes,(AST_INDEX node, int *size));
+STATIC(int, InitializeNodeMapping,(AST_INDEX node, Recurrence *R));
 
 #endif
